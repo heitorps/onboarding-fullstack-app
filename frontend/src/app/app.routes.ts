94368770beb484ac8bbad0fdc4f2bf6f7714a608
heitorps <heitorps2020@gmail.com>
2026-08-05 11:41:18 -0300

    iniciando perfil

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Auth } from './pages/auth/auth'
+import { Timeline } from './pages/timeline/timeline';
+import { Profile } from './pages/profile/profile';
+import { authGuard } from './services/auth.guard';
+import { ReviewDetailed } from './pages/review-detailed/review-detailed';
+
+export const routes: Routes = [
+    {path: '', component: Auth, pathMatch: 'full'},
+    {path: 'timeline', component: Timeline, canActivate: [authGuard]},
+    {path: 'profile/:id', component: Profile, canActivate: [authGuard]},
+    {path: 'review/:reviewId', component: ReviewDetailed, canActivate: [authGuard]},
+    {path: '**', redirectTo: ''}
+];
